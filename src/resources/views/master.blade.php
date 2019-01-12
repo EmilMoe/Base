2a240ace3b17233e874816bb5165279f7475782c
Emil Moe <emil@moegroup.dk>
2019-01-12 20:05:54 +0100

    Added proper support for left menu.

--- src/resources/views/master.blade.php
+++ src/resources/views/master.blade.php
@@ -7,6 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link xmlns="https://www.w3.org/1999/xhtml" rel="shortcut icon" href="{{ url('images/logo.svg') }}">
         <link rel="manifest" href="{{ url('mix-manifest.json') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
 
         <title>{{ isset($pagename) ? $pagename : env('APP_NAME') }}</title>
 
@@ -14,10 +15,14 @@
     </head>
     <body class="{{ $class['body'] ?? '' }}">
         <div id="app" v-cloak>
-            @include('EmilMoe\Navigation::left')
+            @if(View::exists('EmilMoe\Navigation::left') && $menu !== false)
+                @include('EmilMoe\Navigation::left')
+            @endif
+
             <main>
                 @yield('page')
             </main>
+            
             <footer>
             </footer>
         </div>
