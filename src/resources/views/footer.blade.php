1ca991308d58900a4a6ef484c242803e5d08be46
Emil Moe <emil@moegroup.dk>
2019-10-01 13:45:39 +0200

    footer

--- src/resources/views/footer.blade.php
+++ src/resources/views/footer.blade.php
@@ -1,7 +1,3 @@
-<footer class="footer mt-auto">
-    <nav class="navbar navbar-light">
-        <div class="container-fluid offset-xl-1 col-xl-8">
-            <span class="navbar-brand mb-0 h1">Navbar</span>
-        </div>
-    </nav>
+<footer class="small text-center">
+    Copyright {{ env('APP_NAME') }} &copy; {{ date('Y') }}
 </footer>
--- src/resources/views/master-admin.blade.php
+++ src/resources/views/master-admin.blade.php
@@ -1,38 +1,40 @@
 @extends('EmilMoe\Base::master')
 
 @section('site')
-    @include('EmilMoe\Base::navbar')
+    <div class="page-container">
+        @include('EmilMoe\Base::navbar')
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-2 offset-xl-1 col-md-2">
-                @include('EmilMoe\Base::navigation')
-            </div>
-            <div class="col-xl-8 col-md-10">
-                @if(isset($pagetitle))
-                    <div class="row">
-                        <div class="col">
-                            <h2 class="col-title">
-                                {{ $pagetitle }}
-                            </h2>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-2 offset-xl-1 col-md-2">
+                    @include('EmilMoe\Base::navigation')
+                </div>
+                <div class="col-xl-8 col-md-10">
+                    @if(isset($pagetitle))
+                        <div class="row">
+                            <div class="col">
+                                <h2 class="col-title">
+                                    {{ $pagetitle }}
+                                </h2>
+                            </div>
                         </div>
-                    </div>
-                @endif
+                    @endif
 
-                @if(isset($breadcrumb) && is_array($breadcrumb))
-                    <div class="row">
-                        <div class="col">
-                            @include('EmilMoe\Base::breadcrumb')
+                    @if(isset($breadcrumb) && is_array($breadcrumb))
+                        <div class="row">
+                            <div class="col">
+                                @include('EmilMoe\Base::breadcrumb')
+                            </div>
                         </div>
+                    @endif
+
+                    <div id="loader-container">
+                        @yield('page')
                     </div>
-                @endif
-                
-                <div id="loader-container">
-                    @yield('page')
                 </div>
             </div>
         </div>
-    </div>
 
-    @include('EmilMoe\Base::footer')
+        @include('EmilMoe\Base::footer')
+    </div>
 @endsection
